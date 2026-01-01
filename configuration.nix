36350d8c87b434dc19834c4919c3c447acaa3bd4
paredesg <paredes_g@hotmail.com>
2026-01-01 21:03:24 +0100

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,23 +1,30 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
-
+  
 {
+
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
+  nix.settings = {
+    download-buffer-size = 4194304000 ; # 4GB
+  };
+
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  
-  networking.hostName = "nixos-vm"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  networking.hostName = "nixos-vm";
+  networking.networkmanager.enable = true;
+
+  # disable wireless for conflit with networkmanager
+  networking.wireless.enable = false;
+
+  # Enable bluetooth.
+  #hardware.bluetooth.enable = true; # enables support for Bluetooth
+  #hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  #services.blueman.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Paris";
@@ -25,7 +32,32 @@
   # autorise installation packages proprietaire
   nixpkgs.config.allowUnfree = true;
 
-  #SSH 
+  # Enable the LY display manager.
+  services.displayManager.ly.enable = true;
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
+    autoRepeatDelay = 200;
+    autoRepeatInterval = 35;
+    windowManager.qtile.enable = true;
+    xkb.layout = "fr";
+    displayManager.sessionCommands = ''
+      xwallpaper --zoom ~/nixos-dotfiles/walls/wall1.png
+    '';
+    extraConfig = ''
+      	Section "Monitor"
+      	  Identifier "Virtual-1"
+      	  Option "PreferredMode" "1920x1080"
+      	EndSection
+    '';
+  };
+
+  # Enable the Picom compositor.
+  services.picom.enable = true;
+
+  #SSH
   services.openssh = {
     enable = true;
     ports = [ 22 ];
@@ -35,78 +67,50 @@
     };
   };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
-    i18n.defaultLocale = "fr_FR.UTF-8";
+  i18n.defaultLocale = "fr_FR.UTF-8";
+  i18n.extraLocaleSettings = {
+  LC_CTYPE = "fr_FR.UTF8";
+  LC_ADDRESS = "fr_FR.UTF-8";
+  LC_MEASUREMENT = "fr_FR.UTF-8";
+  LC_MESSAGES = "fr_FR.UTF-8";
+  LC_MONETARY = "fr_FR.UTF-8";
+  LC_NAME = "fr_FR.UTF-8";
+  LC_NUMERIC = "fr_FR.UTF-8";
+  LC_PAPER = "fr_FR.UTF-8";
+  LC_TELEPHONE = "fr_FR.UTF-8";
+  LC_TIME = "fr_FR.UTF-8";
+  LC_COLLATE = "fr_FR.UTF-8";
+  };
 
-    i18n.extraLocaleSettings = {
-    # LC_ALL = "fr_FR.UTF-8"; # This overrides all other LC_* settings.
-    LC_CTYPE = "fr_FR.UTF8";
-    LC_ADDRESS = "fr_FR.UTF-8";
-    LC_MEASUREMENT = "fr_FR.UTF-8";
-    LC_MESSAGES = "fr_FR.UTF-8";
-    LC_MONETARY = "fr_FR.UTF-8";
-    LC_NAME = "fr_FR.UTF-8";
-    LC_NUMERIC = "fr_FR.UTF-8";
-    LC_PAPER = "fr_FR.UTF-8";
-    LC_TELEPHONE = "fr_FR.UTF-8";
-    LC_TIME = "fr_FR.UTF-8";
-    LC_COLLATE = "fr_FR.UTF-8";
-    };
-
-#    console = {
-#      font = "Lat2-Terminus16";
-#      keyMap = "fr";
-#      useXkbConfig = true; # use xkb.options in tty.
-#    };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-	services.xserver = {
-		enable = true;
-		windowManager.qtile.enable = true;
-        xkb.layout = "fr";
-	
-	};
-
-	#services.picom = {
-	#	enable = true;
-	#	backend = "glx";
-	#	fade = true;
-	#};
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.willy = {
+  # Define a user account. Don't forget to set a password with `passwd`.
+  users.users.eve = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "docker" "networkmanager" "audio" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       tree
-      bat
-      curl
-      mc
     ];
   };
 
-  programs.firefox.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
-	vim
-	alacritty
-	git
-	pfetch
+    git
+    alacritty
   ];
 
-	fonts.packages = with pkgs; [
-		jetbrains-mono
-	];
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
 
-   system.stateVersion = "25.05"; # Did you read the comment?
+  # garbage collector
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "25.05";
 }
+
